70a9ef853e557ae47a814c3537f190336f7907eb
VladPeskovDev <vladpeskovdev@gmail.com>
2024-08-21 00:43:56 +0300

    createDone

--- server/db/seeders/20240819134245-initial-seed.js
+++ server/db/seeders/20240819134245-initial-seed.js
@@ -1,28 +1,23 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+require('dotenv').config();
 const bcrypt = require('bcrypt');
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Users', [
       {
-      username: 'Лев Тропин',
-      email: "lev@mail.ru",
-      password: await bcrypt.hash('12345', 10),
-      role: 'admin',
-      },
+        username: 'Влад Песков',
+        email: "xfemidaxx@mail.ru",
+        password: await bcrypt.hash(process.env.ADMIN_PASS, 10),
+        role: 'user',
+        },
       {
         username: 'Владислав Песков',
-        email: "xfemidax@mail.ru",
-        password: await bcrypt.hash('123', 10),
+        email: process.env.ADMIN_EMAIL,
+        password: await bcrypt.hash(process.env.ADMIN_PASS, 10),
         role: 'admin',
-        },
-        {
-          username: 'Влад Песков',
-          email: "xfemidaxx@mail.ru",
-          password: await bcrypt.hash('123', 10),
-          role: 'user',
-          }
+        }  
     ], {});
     await queryInterface.bulkInsert(
       'Lawyers', [
--- server/src/routes/lawyerRouter.js
+++ server/src/routes/lawyerRouter.js
@@ -1,5 +1,6 @@
 const lawyerRouter = require('express').Router();
 const { Lawyer, User } = require('../../db/models');
+const { verifyAccessToken } = require('../middlewares/verifyTokens');
 
 lawyerRouter.route('/').get(async (req, res) => {
   try {
@@ -9,5 +10,44 @@ lawyerRouter.route('/').get(async (req, res) => {
     res.status(500).send('Internal server error');
   }
 });
+
+lawyerRouter
+  .route('/')
+  .post(verifyAccessToken, async (req, res) => {
+    try {
+      const userId = res.locals.user.id;
+      if (userId !== 2) {
+        return res.status(403).json({ error: 'You are not authorized to create lawyer posts.' });
+      }
+// Создание карточки адвоката
+      const post = await Lawyer.create({
+        name: req.body.name,
+        education: req.body.education,
+        description: req.body.description,
+        userId,
+        photo: req.body.photo,
+        phone: req.body.phone,
+        telegram: req.body.telegram,
+      });
+
+      const plainX = await Lawyer.findOne({
+        where: {
+          id: post.id,
+        },
+        include: {
+          model: User,
+          as: 'admin',
+          attributes: ['id', 'username', 'email'],
+        },
+      });
+
+      res.json(plainX);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Internal server error');
+    }
+  });
+
+
 
 module.exports = lawyerRouter;
