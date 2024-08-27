1f4afc44d1b439b37979e5da8beb440deb3cf771
VladPeskovDev <vladpeskovdev@gmail.com>
2024-08-27 03:28:51 +0300

    Cases2.0

--- client/src/utils/validatorsCases.ts
+++ client/src/utils/validatorsCases.ts
@@ -4,11 +4,11 @@ export const CaseSchema = z.object({
   id: z.number().optional(), 
   title: z.string(),
   description: z.string(),
-  photo1: z.string().optional(), 
-  photo2: z.string().optional(),
-  photo3: z.string().optional(),
-  photo4: z.string().optional(),
-  photo5: z.string().optional(),
+  photo1: z.string(), 
+  photo2: z.string(),
+  photo3: z.string(),
+  photo4: z.string(),
+  photo5: z.string(),
   userID: z.number().nullable().optional(), // Связанный пользователь может быть отсутствующим
   
 });
